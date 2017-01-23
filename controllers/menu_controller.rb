d2569c3f8b68d8e567a0e9b3ef365393b3447539
Tangati, Lavanya <ltangati@paypal.com>
2017-01-23 13:14:39 -0600

    checkpoint 23 completed

--- controllers/menu_controller.rb
+++ controllers/menu_controller.rb
@@ -74,9 +74,64 @@ class MenuController
     end
 
     def search_entries
+      print "Search by name:"
+      name = gets.chomp
+
+      match = address_book.binary_search(name)
+      system "clear"
+
+      if match
+        puts match.to_s
+        search_submenu(match)
+      else
+        puts "No entry found for #{name}"
+      end
+    end
+
+    def search_submenu
+      puts "/nd - delete the entry"
+      puts "e - edit the entry"
+      puts "m - return to main menu"
+
+      selection = gets.to_i
+      case selection
+        when "d"
+          system "clear"
+          delete_entry(entry)
+          main_menu
+        when "e"
+          system "clear"
+          edit_entry(entry)
+          main_menu
+        when "m"
+          system "clear"
+          main_menu
+        else
+         system "clear"
+         puts "#{selection} is not a valid input"
+         puts entry.to_s
+         search_submenu(entry)
+      end
     end
 
     def read_csv
+      print "Please enter the CSV file name to import"
+      file_name = gets.chomp
+
+      if file_name.empty?
+        system "clear"
+        puts "No CSV file found"
+        main_menu
+      end
+
+      begin
+        entry_count = address_book.import_from_csv(file_name).count
+        system "clear"
+        puts "#{entry_count} new entries added from #{file_name}"
+      rescue
+        puts "#{file_name} is not a valid CSV file. Please enter the name again"
+        read_csv
+      end
     end
 
     def entry_submenu(entry)
@@ -92,8 +147,10 @@ class MenuController
             when "n"
 
             when "d"
+              delete_entry(entry)
             when "e"
-
+              edit_entry(entry)
+              entry_submenu(entry)
             when "m"
                 system "clear"
                 main_menu
@@ -102,5 +159,29 @@ class MenuController
                 puts "#{selection} is not a valid input"
                 entry_submenu(entry)
         end
+    end
+
+    def delete_entry(entry)
+        address_book.entries.delete(entry)
+        puts "#{entry.name} has been deleted"
+    end
+
+    def edit_entry(entry)
+      print "Updated Name:"
+      name = gets.chomp
+
+      print "Updated phone:"
+      phone_number = gets.chomp
+
+      print "Updated email"
+      email = gets.chomp
+
+      entry.name = name if !name.empty?
+      entry.phone_number = phone_number if !phone_number.empty?
+      entry.email = email if !email.empty?
+      system "clear"
+
+      puts "Updated entry:"
+      puts entry
     end
 end
